179836c6f30f14ef0e28f958d2f924178caaac8d
Richard Smith <gsmith7@iastate.edu>
2018-02-05 20:32:26 -0600

    delete

--- Project_1/lab2-0.c
+++
@@ -1,9 +0,0 @@
-// Richard Smith, se185 E, 8/29/2016
-// LAB2-0.c : Defines the entry point for the console application.
-
-#include<stdio.h>
-
-int main(int argc, char* argv []){
-printf("Richard Gabriel Smith, se185 E");
-return 0;
-}
